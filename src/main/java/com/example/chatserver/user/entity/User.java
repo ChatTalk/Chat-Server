403059a24e1def4b2fdb515eb855473e1e3e853e
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-25 21:51:27 +0900

    feat: 회원 엔티티 생성

---
+++ src/main/java/com/example/chatserver/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.chatserver.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+}
