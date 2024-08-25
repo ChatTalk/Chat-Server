610a833a5256b5b6822f11095a14088e3ed8ceea
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-26 00:58:29 +0900

    fix: 필드 저장 타입 변경

--- src/main/java/com/example/chatserver/domain/user/entity/User.java
+++ src/main/java/com/example/chatserver/domain/user/entity/User.java
@@ -23,6 +23,7 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "role")
     private UserRoleEnum role;
 
