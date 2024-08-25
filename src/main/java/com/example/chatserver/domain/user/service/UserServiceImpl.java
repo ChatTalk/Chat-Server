396d34fcd85615ff91ba577cf5176d0f6c092f6d
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-25 22:29:18 +0900

    feat: 어노테이션 할당 및 컨트롤러 세팅

--- src/main/java/com/example/chatserver/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/chatserver/domain/user/service/UserServiceImpl.java
@@ -9,8 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 @Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
