d9ef9086cd56dc4564bdb859aa75707a770a7d98
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-25 21:59:04 +0900

    feat: 회원 엔티티 DAO 작성

---
+++ src/main/java/com/example/chatserver/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.chatserver.domain.user.repository;
+
+import com.example.chatserver.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
