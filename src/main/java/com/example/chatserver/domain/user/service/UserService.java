14cd4b4f0bd6a2cd94c372e22cad6896ab942fbc
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-26 02:28:41 +0900

    feat: 시큐리티 설정 클래스 작성

--- src/main/java/com/example/chatserver/domain/user/service/UserService.java
+++ src/main/java/com/example/chatserver/domain/user/service/UserService.java
@@ -2,9 +2,7 @@ package com.example.chatserver.domain.user.service;
 
 import com.example.chatserver.domain.user.dto.UserDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService extends UserDetailsService {
 
     UserDTO createUser(UserDTO userDto);
--- src/main/java/com/example/chatserver/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/chatserver/global/config/WebSecurityConfig.java
@@ -1,20 +1,36 @@
 package com.example.chatserver.global.config;
 
+import com.example.chatserver.domain.user.service.UserService;
+import com.example.chatserver.global.security.exception.JwtAccessDenyHandler;
+import com.example.chatserver.global.security.exception.JwtAuthenticationEntryPoint;
+import com.example.chatserver.global.security.filter.CustomLoginFilter;
+import com.example.chatserver.global.security.filter.JwtAuthenticationFilter;
+import com.example.chatserver.global.security.filter.JwtAuthorizationFilter;
+import com.example.chatserver.global.security.filter.JwtExceptionFilter;
+import com.example.chatserver.global.security.service.JwtTokenService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+
+import static com.example.chatserver.global.constant.Constants.COOKIE_AUTH_HEADER;
 
 @Configuration
 @EnableWebSecurity
@@ -25,17 +41,68 @@ public class WebSecurityConfig {
     @Value("${client.url}")
     private String clientUrl;
 
+    private final UserService userService;
+    private final UserDetailsService userDetailsService;
+    private final JwtTokenService jwtTokenService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    // 필터단 예외
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint; // 인증 예외 커스텀 메시지 던지기
+    private final JwtAccessDenyHandler jwtAccessDenyHandler; // 인가 예외 커스텀 메시지 던지기(역할별 접근권한같은)
+    private final JwtExceptionFilter jwtExceptionFilter;
+
+    // 인증 매니저 생성
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CustomLoginFilter customLoginFilter() throws Exception {
+        CustomLoginFilter filter = new CustomLoginFilter(jwtTokenService);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);  // csrf 토큰 무효화 설정을 해야 인증 예외 허용 가능
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         // cors 설정이랑 WebMvc 에서의 cors 설정이 충돌할 수 있기 때문에 security 단계에서만의 설정 필요
 
+        // Security 의 기본 설정인 Session 방식이 아닌 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement(sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        //만약 권한이 없는 상태에서 바로 권한 요청을 하는 경우 처리
+        http.exceptionHandling(e ->
+                e.authenticationEntryPoint(jwtAuthenticationEntryPoint).accessDeniedHandler(jwtAccessDenyHandler));
+
+        // JWT 방식의 REST API 서버이기 때문에 FormLogin 방식, HttpBasic 방식 비활성화
+        // 클라이언트가 분리됐으므로 비할성화
+        http.formLogin(AbstractHttpConfigurer::disable);
+
         http.authorizeHttpRequests(authorizeHttpRequests ->
                 authorizeHttpRequests
                         .requestMatchers(HttpMethod.POST, "/api/users/signup").permitAll()
                         .anyRequest().authenticated()
         );
+
+        http.logout(logout -> logout
+                .logoutUrl("/api/users/logout")  // 로그아웃 API 엔드포인트
+                .logoutSuccessHandler((req, res, auth) -> {
+                    res.setStatus(HttpServletResponse.SC_OK);  // 로그아웃 성공 시 200 OK 응답
+                })
+                .deleteCookies(COOKIE_AUTH_HEADER)
+        );
+
+        // 필터 체인에 필터 추가 및 순서 지정
+        http.addFilterBefore(new JwtAuthorizationFilter(),
+                CustomLoginFilter.class);
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenService, userService, userDetailsService), JwtAuthorizationFilter.class);
+        http.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+        http.addFilterBefore(customLoginFilter(), UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
