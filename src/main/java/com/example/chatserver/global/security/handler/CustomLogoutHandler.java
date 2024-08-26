b3397b04f46a24c39726eed28788af272099bb87
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-26 14:48:47 +0900

    feat: 만료 토큰에서의 로그아웃 로직 보강

--- src/main/java/com/example/chatserver/global/security/handler/CustomLogoutHandler.java
+++ src/main/java/com/example/chatserver/global/security/handler/CustomLogoutHandler.java
@@ -1,6 +1,7 @@
 package com.example.chatserver.global.security.handler;
 
 import com.example.chatserver.global.security.service.JwtTokenService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -37,10 +38,16 @@ public class CustomLogoutHandler implements LogoutHandler {
             if (cookie.getName().equals(COOKIE_AUTH_HEADER)) {
                 String decodedToken = URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8);
                 try {
+                    log.info("정상 토큰에서의 로그아웃 처리");
                     String email = jwtTokenService.getUserFromToken(decodedToken.substring(7)).getEmail();
+                    redisTemplate.delete(REDIS_REFRESH_KEY + email);
+                } catch (ExpiredJwtException e) {
+                    log.warn("만료 토큰에서의 로그아웃 처리");
+                    String email = jwtTokenService.getUsernameFromExpiredJwt(e);
                     redisTemplate.delete(REDIS_REFRESH_KEY + email);
                 } catch (Exception e) {
                     log.error("Redis에서 리프레시 토큰 삭제 중 오류 발생", e);
+                    throw e;
                 }
                 break;
             }
