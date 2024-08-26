43ee7d5bd16950b29582f60ecb714fcf9c4bad80
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-27 00:40:58 +0900

    feat: 채팅 메세지 도큐먼트 생성

---
+++ src/main/java/com/example/chatserver/domain/chat/document/ChatMessage.java
@@ -0,0 +1,38 @@
+package com.example.chatserver.domain.chat.document;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Document(collection = "chatMessage")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id; // 도큐먼트의 아이디
+    
+    private String chatId; // 채팅방 아이
+    
+    private String username; // 메세지 전송인
+
+    private String message; // 메세지
+    
+    private String createdAt;
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public ChatMessage(String chatId, String username, String message) {
+        this.chatId = chatId;
+        this.username = username;
+        this.message = message;
+        this.createdAt = LocalDateTime.now().format(FORMATTER);
+    }
+}
