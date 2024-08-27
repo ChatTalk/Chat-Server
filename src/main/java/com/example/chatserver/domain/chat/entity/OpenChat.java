6cea294619d8a8b06bda18293a96ad28427f1adc
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-28 00:49:01 +0900

    fix: 엔티티 생성시간 재설정

--- src/main/java/com/example/chatserver/domain/chat/entity/OpenChat.java
+++ src/main/java/com/example/chatserver/domain/chat/entity/OpenChat.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "openchats")
+@EntityListeners(AuditingEntityListener.class)
 public class OpenChat {
 
     @Id
