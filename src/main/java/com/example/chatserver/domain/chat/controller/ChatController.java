8ad37d67bde019fb4984580d6fe77755f1b5a15e
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-27 20:47:48 +0900

    fix: 구독 해제 로직 순서 조정

--- src/main/java/com/example/chatserver/domain/chat/controller/ChatController.java
+++ src/main/java/com/example/chatserver/domain/chat/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.example.chatserver.domain.chat.controller;
 
 import com.example.chatserver.domain.chat.document.ChatMessage;
 import com.example.chatserver.domain.chat.dto.ChatMessageDTO;
+import com.example.chatserver.domain.chat.service.RedisSubscriber;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -18,12 +19,16 @@ import static com.example.chatserver.global.constant.Constants.REDIS_CHAT_PREFIX
 public class ChatController {
 
     private final RedisTemplate<String, ChatMessageDTO> messageTemplate;
+    private final RedisSubscriber redisSubscriber;
 
     // 사용자의 채팅방 입장
     @MessageMapping(value = "/chat/enter")
     public void enter(ChatMessageDTO.Enter enter, Principal principal) {
         log.info("{}반 채팅방에서 클라이언트로부터 {} 회원이 입장 요청",
                 enter.getChatId(), principal.getName());
+
+        // 채팅방 구독
+        redisSubscriber.subscribe(REDIS_CHAT_PREFIX + enter.getChatId());
 
         // 채팅 메세지 엔티티 관련 서비스 로직(mongoDB) 수행
         ChatMessage message = new ChatMessage(enter, principal.getName());
@@ -56,5 +61,8 @@ public class ChatController {
 
         ChatMessageDTO dto = new ChatMessageDTO(message);
         messageTemplate.convertAndSend(REDIS_CHAT_PREFIX + message.getChatId(), dto);
+
+        // 메세지 전송 완료 후, 채팅방 구독 해제
+        redisSubscriber.unsubscribe(REDIS_CHAT_PREFIX + leave.getChatId());
     }
 }
